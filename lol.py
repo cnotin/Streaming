88b4eac9ebf26beddf6aff7c954bf8a9c1a1c634
ClementNotin <clement@notin.org>
2011-02-23 17:48:56 +0100

    initial commit

---
+++ lol.py
@@ -0,0 +1,31 @@
+class Catalogue():
+        def __init__(self, fichier):
+                self.fichier = fichier
+                self.objects = []
+
+                f = open(self.fichier, "r")
+                temp = f.readlines()
+                self.servAddr = temp[0].split(": ")[1].strip()
+                self.servPort = int(temp[1].split(": ")[1])
+                for line in temp[2:]:
+                        id = int(line.split("=")[1].split(" ")[0])
+                        name = line.split("=")[2].split(" ")[0]
+                        type = line.split("=")[3].split(" ")[0]
+                        addr = line.split("=")[4].split(" ")[0]
+                        port = int(line.split("=")[5].split(" ")[0])
+                        protocole = line.split("=")[6].split(" ")[0]
+                        ips = float(line.split("=")[7].split(" ")[0])
+                        self.objects.append((id, name, type, addr, port, protocole, ips))
+
+
+
+        def format(self):
+                retour = "ServerAddress: " + self.servAddr + "\r\n"
+                retour += "ServerPort: " + str(self.servPort) + "\r\n"
+                for objet in self.objects:
+                        retour += "Object ID=" + str(objet[0]) + " name=" + objet[1] + " type=" + objet[2] + " address=" + \
+                        objet[3] + " port=" + str(objet[4]) + " protocol=" + objet[5] + " ips=" + str(objet[6]) + "\r\n"
+                print retour
+
+cat = Catalogue("catalogue.txt")
+cat.format()
---
+++ streaming.py
@@ -0,0 +1,38 @@
+﻿from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
+from twisted.protocols.basic import LineReceiver
+from catalogue import Catalogue
+
+
+class ServeurHttp(LineReceiver):
+        def __init__(self):
+                self.cat = Catalogue("catalogue.txt")
+
+        def __del__(self):
+                pass
+
+        def addHeader(self, msg):
+                header = "HTTP/1.1 200 OK\r\nServer: localhost\r\nConnection: Keep-Alive\r\nContent-Type: text/txt\r\nContent-Length: "
+                header += str(len(msg))
+                msg = header + "\r\n\r\n" + msg
+                return msg
+
+        def lineReceived(self, line):
+                if (line.find("GET /catalogue") != -1):
+                        self.transport.write(self.addHeader(self.cat.getCatalogue()))
+                #self.transport.loseConnection()
+
+        def connectionMade(self):
+                print "client connecté"
+
+
+def main():
+        print "Welcome"
+        f = Factory()
+        f.protocol = ServeurHttp
+        reactor.listenTCP(4590, f)
+        #stdio.StandardIO(ServeurHttp())
+        reactor.run()
+
+if __name__ == '__main__':
+        main()
