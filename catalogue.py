e618874913748f356f867271a9cd56e62fe2054b
ClementNotin <clement@notin.org>
2011-03-14 14:19:11 +0100

    spaces -> tab indent

--- catalogue.py
+++ catalogue.py
@@ -1,30 +1,30 @@
 class Catalogue():
-        def __init__(self, fichier):
-                self.fichier = fichier
-                self.objects = []
+	def __init__(self, fichier):
+		self.fichier = fichier
+		self.objects = []
 
-                f = open(self.fichier, "r")
-                temp = f.readlines()
-                self.servAddr = temp[0].split(": ")[1].strip()
-                self.servPort = int(temp[1].split(": ")[1])
-                for line in temp[2:]:
-                        splittedLine = line.split("=")
-                        id = int(splittedLine[1].split(" ")[0])
-                        name = splittedLine[2].split(" ")[0]
-                        type = splittedLine[3].split(" ")[0]
-                        addr = splittedLine[4].split(" ")[0]
-                        port = int(splittedLine[5].split(" ")[0])
-                        protocole = splittedLine[6].split(" ")[0]
-                        ips = float(splittedLine[7].split(" ")[0])
-                        self.objects.append((id, name, type, addr, port, protocole, ips))
+		f = open(self.fichier, "r")
+		temp = f.readlines()
+		self.servAddr = temp[0].split(": ")[1].strip()
+		self.servPort = int(temp[1].split(": ")[1])
+		for line in temp[2:]:
+			splittedLine = line.split("=")
+			id = int(splittedLine[1].split(" ")[0])
+			name = splittedLine[2].split(" ")[0]
+			type = splittedLine[3].split(" ")[0]
+			addr = splittedLine[4].split(" ")[0]
+			port = int(splittedLine[5].split(" ")[0])
+			protocole = splittedLine[6].split(" ")[0]
+			ips = float(splittedLine[7].split(" ")[0])
+			self.objects.append((id, name, type, addr, port, protocole, ips))
 
 
 
-        def getCatalogue(self):
-                retour = "ServerAddress: " + self.servAddr + "\r\n"
-                retour += "ServerPort: " + str(self.servPort) + "\r\n"
-                for objet in self.objects:
-                        retour += "Object ID=" + str(objet[0]) + " name=" + objet[1] + " type=" + objet[2] + " address=" + \
-                        objet[3] + " port=" + str(objet[4]) + " protocol=" + objet[5] + " ips=" + str(objet[6]) + "\r\n"
-                retour += "\r\n"
-                return retour
+	def getCatalogue(self):
+		retour = "ServerAddress: " + self.servAddr + "\r\n"
+		retour += "ServerPort: " + str(self.servPort) + "\r\n"
+		for objet in self.objects:
+			retour += "Object ID=" + str(objet[0]) + " name=" + objet[1] + " type=" + objet[2] + " address=" + \
+			objet[3] + " port=" + str(objet[4]) + " protocol=" + objet[5] + " ips=" + str(objet[6]) + "\r\n"
+		retour += "\r\n"
+		return retour
--- streaming.py
+++ streaming.py
@@ -1,43 +1,44 @@
+# -*- coding: utf-8 -*-
+
 from catalogue import Catalogue
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 
 class ServeurHttp(LineReceiver):
-        def __init__(self):
-                self.delimiter = "\n"
-        def __del__(self):
-                pass
+	def __init__(self):
+		self.delimiter = "\n"
+	def __del__(self):
+		pass
 
-        def addHeader(self, msg):
-                header = "HTTP/1.1 200 OK\r\nServer: localhost\r\nConnection: Keep-Alive\r\nContent-Type: text/txt\r\nContent-Length: "
-                header += str(len(msg))
-                msg = header + "\r\n\r\n" + msg
-                return msg
+	def addHeader(self, msg):
+		header = "HTTP/1.1 200 OK\r\nServer: localhost\r\nConnection: Keep-Alive\r\nContent-Type: text/txt\r\nContent-Length: "
+		header += str(len(msg))
+		msg = header + "\r\n\r\n" + msg
+		return msg
 
-        def lineReceived(self, line):
-                #on a un GET mais pas pour le catalogue => dégage
-                if ((line.find("GET") == 0) and (line.find("GET /catalogue") == -1)):
-                        self.transport.loseConnection()
-                if (line.find("GET /catalogue") != -1):
-                        self.transport.write(self.addHeader(self.factory.cat.getCatalogue()))
+	def lineReceived(self, line):
+		#on a un GET mais pas pour le catalogue => dégage
+		if ((line.find("GET") == 0) and (line.find("GET /catalogue") == -1)):
+			self.transport.loseConnection()
+		if (line.find("GET /catalogue") != -1):
+			self.transport.write(self.addHeader(self.factory.cat.getCatalogue()))
 
-                
+	def connectionMade(self):
+		print "client connecté"
 
-        def connectionMade(self):
-                print "client connecté"
 
 class ServeurHTTPFactory(Factory):
-        protocol = ServeurHttp
+	protocol = ServeurHttp
 
-        def __init__(self):
-                self.cat = Catalogue("catalogue.txt")
+	def __init__(self):
+		self.cat = Catalogue("catalogue.txt")
 
 
 def main():
-        print "Welcome"
-        reactor.listenTCP(4590, ServeurHTTPFactory())
-        reactor.run()
+	print "Welcome"
+	reactor.listenTCP(4590, ServeurHTTPFactory())
+	reactor.run()
 
 if __name__ == '__main__':
-        main()
+	main()
