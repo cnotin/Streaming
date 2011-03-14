11d2494191d4c143fc7b2afe796fe9bea591ac5c
ClementNotin <clement@notin.org>
2011-03-14 17:34:31 +0100

    ça marche + les images

--- lol.py
+++ lol.py
@@ -1,31 +1,19 @@
-class Catalogue():
-        def __init__(self, fichier):
-                self.fichier = fichier
-                self.objects = []
+PRON = "pr0n\\"
+images = []
+images.append("")
 
-                f = open(self.fichier, "r")
-                temp = f.readlines()
-                self.servAddr = temp[0].split(": ")[1].strip()
-                self.servPort = int(temp[1].split(": ")[1])
-                for line in temp[2:]:
-                        id = int(line.split("=")[1].split(" ")[0])
-                        name = line.split("=")[2].split(" ")[0]
-                        type = line.split("=")[3].split(" ")[0]
-                        addr = line.split("=")[4].split(" ")[0]
-                        port = int(line.split("=")[5].split(" ")[0])
-                        protocole = line.split("=")[6].split(" ")[0]
-                        ips = float(line.split("=")[7].split(" ")[0])
-                        self.objects.append((id, name, type, addr, port, protocole, ips))
+for i in range(1,81):
+	print "image %s" % i
+	f = open(PRON + "mavideo\\" + str(i) + ".jpg", "rb")
+	images.append(f.read())
+	f.close()
 
 
+for i in range(1,81):
+	print "image %s" % i
+	#j = 0
+	#for ch in images[i]:
+	#	j = j + 1
 
-        def format(self):
-                retour = "ServerAddress: " + self.servAddr + "\r\n"
-                retour += "ServerPort: " + str(self.servPort) + "\r\n"
-                for objet in self.objects:
-                        retour += "Object ID=" + str(objet[0]) + " name=" + objet[1] + " type=" + objet[2] + " address=" + \
-                        objet[3] + " port=" + str(objet[4]) + " protocol=" + objet[5] + " ips=" + str(objet[6]) + "\r\n"
-                print retour
-
-cat = Catalogue("catalogue.txt")
-cat.format()
+	print "longueur = %s " % len(images[i])
+	raw_input()
--- streaming.py
+++ streaming.py
@@ -2,8 +2,69 @@
 
 from catalogue import Catalogue
 from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint
 from twisted.internet.protocol import Factory
+from twisted.internet.protocol import Protocol
 from twisted.protocols.basic import LineReceiver
+
+PRON = "pr0n\\"
+SEP = "\r\n"
+
+def gotProtocol(p):
+	p.factory.client = p
+
+class PullProtocol(Protocol):
+	def __init__(self):
+		print "creation pull proto"
+		self.image_id = 1
+		self.images = []
+		self.images.append("") #car ceci commence à 0 et la première image a l'index 1
+		for i in range(1,148):
+			print "image %s" % i
+			f = open(PRON + "mavideo\\img" + str(i) + ".jpg", "rb")
+			self.images.append(f.read())
+			f.close()
+		
+	def sendMessage(self, message):
+		self.transport.write(message)
+		
+	def sendCurrentImage(self):
+		if self.image_id == len(self.images):
+			self.image_id = 1
+		print "j'envoie l'image %s" % self.image_id
+		self.sendMessage("%s%s%s%s%s" % (self.image_id, SEP, len(self.images[self.image_id]), SEP,  self.images[self.image_id]))
+		self.image_id += 1
+
+
+class TCPPull(LineReceiver):
+	def __init__(self):
+		pass
+	def __del__(self):
+		pass
+
+	def lineReceived(self, line):
+		print "TCP PULL = " + line
+		if (line.find("LISTEN_PORT") == 0):
+			point = TCP4ClientEndpoint(reactor, self.transport.getPeer().host, int(line.split(" ")[1]))
+			d = point.connect(self.factory.pullFactory)
+			d.addCallback(gotProtocol)
+		elif (line.find("GET -1") == 0):
+			if ("client" in self.factory.pullFactory.__dict__):
+				self.factory.pullFactory.client.sendCurrentImage()
+			
+
+	def connectionMade(self):
+		print "TCP pull connecté !"
+
+class TCPPullFactory(Factory):
+	protocol = TCPPull
+	pullFactory = None
+
+	def __init__(self):
+		self.pullFactory = Factory()
+		self.pullFactory.protocol = PullProtocol
+
+
 
 class ServeurHttp(LineReceiver):
 	def __init__(self):
@@ -18,14 +79,16 @@ class ServeurHttp(LineReceiver):
 		return msg
 
 	def lineReceived(self, line):
+		print "HTTP = " + line
 		#on a un GET mais pas pour le catalogue => dégage
-		if ((line.find("GET") == 0) and (line.find("GET /catalogue") == -1)):
+		if ((line.find("GET") == 0) and (line.find("GET /catalog") == -1)):
 			self.transport.loseConnection()
-		if (line.find("GET /catalogue") != -1):
+		if (line.find("GET /catalog") != -1):
 			self.transport.write(self.addHeader(self.factory.cat.getCatalogue()))
 
 	def connectionMade(self):
 		print "client connecté"
+
 
 
 class ServeurHTTPFactory(Factory):
@@ -33,6 +96,9 @@ class ServeurHTTPFactory(Factory):
 
 	def __init__(self):
 		self.cat = Catalogue("catalogue.txt")
+		for objet in self.cat.objects:
+			if objet[5] == "TCP_PULL":
+				reactor.listenTCP(objet[4], TCPPullFactory())
 
 
 def main():
