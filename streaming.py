2bb0c0729a29c182b6571c0246d58a3c80108220
ClementNotin <clement@notin.org>
2011-03-25 18:08:05 +0100

    Ajout : possibilité de spécifier dans le fichier MYIP.conf la configuration "ip:port" pour le serveur.
    Si le fichier n'existe pas : valeur par défaut = 127.0.0.1:4590

--- streaming.py
+++ streaming.py
@@ -9,11 +9,20 @@ SEP = "\r\n"
 
 
 def main():
-	print "Bonjour, veuillez entrer l'IP locale (ENTREE pour 127.0.0.1"
-	ip = raw_input()
-	if ip == "":
+	print "Bonjour, bienvenue sur le serveur de streaming de Clément Notin et Thomas Piccolo (B3154)"
+	try:
+		ipFile = open("MYIP.conf", "r")
+	except IOError:
+		print "Le fichier de config de l'adresse IP locale, MYIP.conf, n'a pas été trouvé\nPassage à l'IP par défaut : 127.0.0.1 et port 4590"
 		ip = "127.0.0.1"
-	reactor.listenTCP(4590, ServeurHTTPFactory(ip))
+		port = 4590
+	else:
+		(ip, port) = ipFile.readline().split(":")
+		port = int(port)
+		print "j'écoute sur l'adresse IP %s et le port %d" % (ip, port)
+		ipFile.close()
+
+	reactor.listenTCP(port, ServeurHTTPFactory(ip))
 	reactor.run()
 
 if __name__ == '__main__':
