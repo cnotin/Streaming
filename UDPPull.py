a38d94cae9e79d76894635e2371b193773166810
ClementNotin <clement@notin.org>
2011-03-29 00:15:20 +0200

    correction d'un bug (référence vs value)
    quand on supprime client qui pointe vers un certain client du dico des clients, cela supprime la référence mais pas l'objet pointé

--- UDPPull.py
+++ UDPPull.py
@@ -86,7 +86,7 @@ class UDPPullControl(DatagramProtocol):
 				self.transport.getHandle().setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 20000000)
 				#print "image"
 			elif (line.find("END") == 0):
-				del client
+				del self.clients[host+":%s" % port]
 				#self.transport.connect(host,int(line.split(" ")[1])
 				#point = UDP4ClientEndpoint(reactor, self.transport.getPeer().host, int(line.split(" ")[1]))
 				#d = point.connect(self.factory.UDPPullDataFactory)
