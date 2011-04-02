b8f59756860aff6d1db5efab2abfd7729766b4f8
ClementNotin <clement@notin.org>
2011-04-02 17:07:19 +0200

    Documentation (suite) : HTTP.py

--- HTTP.py
+++ HTTP.py
@@ -13,29 +13,42 @@ from twisted.protocols.basic import LineReceiver
 
 
 class ServeurHttp(LineReceiver):
+	"""
+	Le réacteur crée un objet de cette classe par client HTTP.
+	Hérite du protocole LineReceiver : on reçoit les messages ligne par ligne et le réacteur n'appelle
+	pas les callbacks tant qu'une ligne entière n'a pas été reçue.
+	"""
 	def __init__(self):
+		# Dit au protocole LineReceiver quel est le séparateur de fin de ligne.
 		self.delimiter = "\n"
-	def __del__(self):
-		pass
-
+		
 	def addHeader(self, msg):
+		"""
+		Ajouter le header HTTP minimaliste au message que l'on envoie en réponse en calculant le
+		Content-length à la volée
+		"""
+		
 		header = "HTTP/1.1 200 OK" + SEP + "Server: localhost" + SEP + "Connection: Keep-Alive" + SEP + "Content-Type: text/txt" + SEP + "Content-Length: "
 		header += str(len(msg))
 		msg = header + SEP + SEP + msg
 		return msg
 
 	def lineReceived(self, line):
+		"""
+		Callback appelé à chaque ligne entière reçue
+		"""
 		print "[HTTP] reçu = " + line
-		if (line.find("GET") != -1):
+		if (line.find("GET") != -1): # si la ligne commence par "GET", on envoie le catalogue
 			self.transport.write(self.addHeader(self.factory.cat.getCatalogue()))
 
 	def connectionMade(self):
 		print "[HTTP] client connecté"
 
+		
 
 class ServeurHTTPFactory(Factory):
 	"""
-	Factory qui va créer des instances (une seule ici) d'un serveur HTTP minimaliste,
+	Factory qui va créer des instances par client d'un serveur HTTP minimaliste,
 	la Factory sert à rassembler le code et les attributs qui sont communs entre les différentes instances.
 	Comme par ex le catalogue.
 	"""
